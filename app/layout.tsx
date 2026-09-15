22a39b9d7099aeaa6bee191531c02c0641a52323
unknown <david.rowlands@live.com>
2026-09-16 01:49:51 +0900

    previous

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,7 +151,7 @@ openGraph: {
   type: 'website',
   images: [
     {
-      url: '/og-default.jpg', // Replace with your default image filename in /public
+      url: '/og-default2.jpg', // Replace with your default image filename in /public
       width: 1200,
       height: 630,
       alt: '大阪梅田ドクターズ結婚相談所',
